aa7b75f8f5001ed079cc30c2e879f6fbe6046910
Marlon Demas <marlon@specno.com>
2022-12-05 11:36:32 +0200

    setup firebase hosting and firebase automatic deploys with github

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,12 @@
 export const environment = {
   production: false,
-  firebase: {},
+  firebase: {
+    apiKey: "AIzaSyBu3YgCCeYxigvvzulDVp123O7Cbob-clI",
+    authDomain: "specno-office-space-4c0cf.firebaseapp.com",
+    projectId: "specno-office-space-4c0cf",
+    storageBucket: "specno-office-space-4c0cf.appspot.com",
+    messagingSenderId: "734939541538",
+    appId: "1:734939541538:web:c94d26a9f70aa6bf625a7b",
+    measurementId: "G-656BB7XK4C"
+  },
 };
